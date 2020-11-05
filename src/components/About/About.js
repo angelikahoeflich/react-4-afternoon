fedce0b2c3b7dd9a2ec1fdf8755cce1277e8b609
Angelika Hoeflich <angelikahoeflich@Angelikas-MacBook-Air.local>
2020-11-05 14:30:00 -0800

    extra /> in the code

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -40,7 +40,7 @@ export default class About extends Component {
                   </p>
                 </div>
               )}
-            />/>
+            />
 
           </Switch>
         </div>
