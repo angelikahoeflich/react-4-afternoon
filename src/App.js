38b8bc956c617640b481b91cc13ff510a58942ff
Angelika Hoeflich <angelikahoeflich@Angelikas-MacBook-Air.local>
2020-11-05 12:49:12 -0800

    added links to home and about

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { HashRouter } from 'react-router-dom';
-import routes from './routes'
+import routes from './routes';
+
 
 export default class App extends Component {
   render() {
